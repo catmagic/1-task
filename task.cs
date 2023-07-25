9bc08dc08338700c444ad170d3d605069163b147
Сat Cheshire <catmagic000@gmail.com>
2023-07-25 13:07:13 +0300

    generator random string with fixed size

--- task.cs
+++ task.cs
@@ -1,6 +1,22 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    Console.WriteLine(generateRandomStringWithSize(10));
+  }
+  static char GetLetter()
+{
+    string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+    Random rand = new Random();
+    int num = rand.Next(0, chars.Length);
+    return chars[num];
+}
+  static string generateRandomStringWithSize(int size)
+  {
+      string s = "";
+      for(int i=0;i<size;++i)
+      {
+          s+=GetLetter();
+      }
+      return s;
   }
 }
